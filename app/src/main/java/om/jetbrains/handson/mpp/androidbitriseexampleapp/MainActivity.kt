44dd0b1541e465da10d5494c12953a5b952a829c
Nimish Nandwana <nimish@grappus.com>
2020-07-30 18:11:59 +0530

    feat(*): Initial Commit

---
+++ app/src/main/java/om/jetbrains/handson/mpp/androidbitriseexampleapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package om.jetbrains.handson.mpp.androidbitriseexampleapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
